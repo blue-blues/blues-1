fe8eb288281b165835a9e3cdaba06fe867c19f2d
blues-pc <blues@vaibhavgavade.com>
2024-08-27 00:53:14 +0530

    final data + token

--- dataset_tonken.py
+++ dataset_tonken.py
@@ -1,46 +1,53 @@
+
 import os
 import multiprocessing as mp
 import numpy as np
 import tiktoken
-from datasets import load_dataset
-from tqdm import tqdm 
+from datasets import load_dataset # pip install datasets
+from tqdm import tqdm # pip install tqdm
 
-local_dir = "data/CC-MAIN-2024-10"
-remote_name = "sample-100BT"
-shard_size = int(1e8) #~ 100 million
+# ------------------------------------------
 
-name_text = "text"
+fw = load_dataset("HiTZ/Multilingual-Medical-Corpus",split="train")
 
 
+local_dir = remote_name = "HiTZ/Multilingual-Medical-Corpus"
+
+
+shard_size = int(1e8) # 100M tokens per shard, total of 100 shards
+
+# create the cache the local directory if it doesn't exist yet
 DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
 os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 
+from datasets import load_dataset
 
-fw = load_dataset("HuggingFaceFW/fineweb-edu", name="CC-MAIN-2024-10", split="train")
-
-
-
+# init the tokenizer
 enc = tiktoken.get_encoding("o200k_base")
-eot = enc._special_tokens['<|endoftext|>'] 
+eot = enc._special_tokens['<|endoftext|>'] # end of text token
 def tokenize(doc):
-    tokens = [eot] 
-    tokens.extend(enc.encode_ordinary(doc[name_text]))
+    # tokenizes a single document and returns a numpy array of uint16 tokens
+    tokens = [eot] # the special <|endoftext|> token delimits all documents
+    tokens.extend(enc.encode_ordinary(doc["text"]))
     tokens_np = np.array(tokens)
-    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
-    tokens_np_uint16 = tokens_np.astype(np.uint16)
-    return tokens_np_uint16
+    assert (0 <= tokens_np).all() and (tokens_np < 2**32).all(), "token dictionary too large for uint16"
+    tokens_np_int32 = tokens_np.astype(np.int32)
+    return tokens_np_int32
 
+def write_datafile(filename, tokens_np):
+    np.save(filename, tokens_np)
 
-
-
+# tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
 nprocs = max(1, os.cpu_count()//2)
 with mp.Pool(nprocs) as pool:
     shard_index = 0
+    # preallocate buffer to hold current shard
     all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
     token_count = 0
     progress_bar = None
     for tokens in pool.imap(tokenize, fw, chunksize=16):
 
+        # is there enough space in the current shard for the new tokens?
         if token_count + len(tokens) < shard_size:
             # simply append tokens to current shard
             all_tokens_np[token_count:token_count+len(tokens)] = tokens
@@ -52,7 +59,7 @@ with mp.Pool(nprocs) as pool:
         else:
             # write the current shard and start a new one
             split = "val" if shard_index == 0 else "train"
-            filename = os.path.join(DATA_CACHE_DIR, f"{remote_name}_{split}_{shard_index:06d}")
+            filename = os.path.join(DATA_CACHE_DIR, f"HiTZ_{split}_{shard_index:06d}")
             # split the document into whatever fits in this shard; the remainder goes to next one
             remainder = shard_size - token_count
             progress_bar.update(remainder)
@@ -67,5 +74,5 @@ with mp.Pool(nprocs) as pool:
     # write any remaining tokens as the last shard
     if token_count != 0:
         split = "val" if shard_index == 0 else "train"
-        filename = os.path.join(DATA_CACHE_DIR, f"{remote_name}_{split}_{shard_index:06d}")
+        filename = os.path.join(DATA_CACHE_DIR, f"HiTZ_{split}_{shard_index:06d}")
         write_datafile(filename, all_tokens_np[:token_count])
