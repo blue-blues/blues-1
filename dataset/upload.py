3005ae8a0141c32a46d7cee841d464269cf706ef
blues-pc <blues@vaibhavgavade.com>
2024-08-29 23:31:25 +0530

    upload update

--- dataset/upload.py
+++ dataset/upload.py
@@ -1,11 +1,14 @@
 from huggingface_hub import login
+from huggingface_hub import HfApi
+api = HfApi()
+
 
 login(token='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')
 
 
 
 api.upload_folder(
-    folder_path="dataset/HiTZ/Multilingual-Medical-Corpus",
-    repo_id="blue-blue/medical_dataset_shards",
-    repo_type="dataset",
+    folder_path="/root/blues-1/log",
+    repo_id="blue-blue/05000_medical",
+    repo_type="model",
 )
